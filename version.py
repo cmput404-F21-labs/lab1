405e760c01dd47e6e119aba23ac890245add4847
wowikc <bondarau@ualberta.ca>
2021-09-06 21:46:27 -0400

    script downloads itself

--- version.py
+++ version.py
@@ -1,4 +1,5 @@
 import requests
 
 # print(requests.__version__)
-print(requests.get('https://www.google.com/'))
+r = requests.get('https://raw.githubusercontent.com/cmput404-F21-labs/lab1/master/version.py')
+print(r.text)
